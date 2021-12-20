1e16878eb380d45eea08e0e2746bb3070a8fbe11
Narendra <kumar.mail017@gmail.com>
2021-12-20 20:48:37 +0530

    Docker Prod And Dev Mode Done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2><p>{` Hello Welcome To ${process.env.REACT_APP_NAME} Docker Devlopment`}</p></h2>
         <a
           className="App-link"
           href="https://reactjs.org"
